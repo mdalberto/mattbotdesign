c56434ac3ca3c7fa5e71bf1cb9d5ba65078e8876
mdalberto <mdal138@gmail.com>
2023-09-01 16:19:50 -0400

    11ty version

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+// file that gets used by 11ty compiling
+ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
+
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addWatchTarget("src/css/");
+     eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
+    //   // The base URL: defaults to Path Prefix
+      // baseHref: "buckley-dev/",
+       baseHref: "",
+       extensions: "html",
+     });
+
+    return {
+        dir: {
+          input: 'src',
+          includes: '_includes',
+          output: '_site',
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+      };
+
+
+}
